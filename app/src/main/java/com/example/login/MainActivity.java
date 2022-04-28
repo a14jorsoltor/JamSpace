122404f71161781c5a56764c7d10973481c38523
ausias <a14jorsoltor@inspedralbes.cat>
2022-04-28 09:02:56 +0200

    arreglar la pagina principal

--- app/src/main/java/com/example/login/MainActivity.java
+++ app/src/main/java/com/example/login/MainActivity.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -12,6 +13,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Toast.makeText(getApplicationContext(),"Pantalla Login",Toast.LENGTH_SHORT).show();
+
 
 
 
--- app/src/main/java/com/example/login/PantallaRegistre.java
+++ app/src/main/java/com/example/login/PantallaRegistre.java
@@ -2,7 +2,9 @@ package com.example.login;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Toast;
 
 public class PantallaRegistre extends AppCompatActivity {
@@ -14,5 +16,14 @@ public class PantallaRegistre extends AppCompatActivity {
         Toast.makeText(getApplicationContext(),"Pantalla registre",Toast.LENGTH_SHORT).show();
 
 
+    }
+
+    public void returnLogin(View view) {
+        Intent switchActivityIntent = new Intent(this, MainActivity.class);
+        startActivity(switchActivityIntent);
+    }
+
+    public void Register(View view) {
+        //Aqui van guardar les dades en el firebase
     }
 }
