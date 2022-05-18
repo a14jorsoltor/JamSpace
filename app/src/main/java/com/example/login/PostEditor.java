02c1e3e078f9d0a9f6c6598d2d8412c4457b3c11
ausias <a14jorsoltor@inspedralbes.cat>
2022-05-18 14:18:36 +0200

    ja es pujen imatges

--- app/src/main/java/com/example/login/PostEditor.java
+++ app/src/main/java/com/example/login/PostEditor.java
@@ -1,14 +1,98 @@
 package com.example.login;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.ProgressDialog;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.material.snackbar.Snackbar;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.OnProgressListener;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
+
+import java.util.UUID;
 
 public class PostEditor extends AppCompatActivity {
+
+
+     public Uri imageUri;
+     private FirebaseStorage storage;
+     private StorageReference storageReference;
+     public ImageView iconGame;
+
+    public PostEditor() {
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_post_editor);
+
+
+        iconGame = findViewById(R.id.iconeJoc);
+        storage = FirebaseStorage.getInstance();
+        storageReference = storage.getReference();
+
+        iconGame.setOnClickListener(v -> choosePicture());
+
+    }
+
+    private void choosePicture() {
+        Intent intent = new Intent();
+        intent.setType("image/*");
+        intent.setAction(Intent.ACTION_GET_CONTENT);
+        startActivityForResult(intent, 1);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if(requestCode==1&&resultCode==RESULT_OK&&data!=null&&data.getData()!=null){
+            imageUri=data.getData();
+            iconGame.setImageURI(imageUri);
+            uploadPicture();
+        }
+    }
+
+    private void uploadPicture() {
+
+        final ProgressDialog pd = new ProgressDialog(this);
+        pd.setTitle("Pujant Imatge....");
+        pd.show();
+        final String randomKey= UUID.randomUUID().toString();
+        StorageReference riversRef = storageReference.child("images/");
+        riversRef.putFile(imageUri)
+                .addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+                    @Override
+                    public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+                        pd.dismiss();
+                        // Get a URL to the uploaded content
+                        Snackbar.make(findViewById(android.R.id.content), "imatge pujada", Snackbar.LENGTH_LONG).show();
+                    }
+                })
+                .addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception exception) {
+                        pd.dismiss();
+                        Toast.makeText(getApplicationContext(), "Pujada fallida", Toast.LENGTH_LONG).show();
+                    }
+                })
+                .addOnProgressListener(new OnProgressListener<UploadTask.TaskSnapshot>() {
+                    @Override
+                    public void onProgress(@NonNull UploadTask.TaskSnapshot snapshot) {
+                        double progresPercent =(100.00 * snapshot.getBytesTransferred()/ snapshot.getTotalByteCount());
+                        pd.setMessage("Percent: " + (int) progresPercent + "%");
+                    }
+                });
     }
 }
